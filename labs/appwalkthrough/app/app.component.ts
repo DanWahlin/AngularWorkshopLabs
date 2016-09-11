01a3612de961393ea26d134d1e73f9d67dfd614f
Dan Wahlin <dwahlin@xmlforasp.net>
2016-09-11 15:35:56 -0700

    Added app walkthrough lab

---
+++ labs/appwalkthrough/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({ 
+  selector: 'app-component',
+  template: `<h1>Welcome to Angular 2!</h1>`
+})
+export class AppComponent implements OnInit {
+  
+  constructor() {  }
+  
+  ngOnInit() {
+
+  }
+  
+}
