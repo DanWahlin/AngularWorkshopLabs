5cbcc6e4dcfe00b8a2d7d22434a5bfab8deb1633
Dan Wahlin <dwahlin@xmlforasp.net>
2016-10-25 00:47:15 -0700

    Updated plunkers

--- labs/startertemplate/app/app.module.ts
+++ labs/startertemplate/app/app.module.ts
@@ -4,7 +4,6 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule} from '@angular/http';
 
 import { AppComponent }  from './app.component';
-import { app_routing } from './app.routing';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, HttpModule ],
--- labs/startertemplate/app/app.routing.ts
+++
@@ -1,7 +0,0 @@
-import { RouterModule, Routes } from '@angular/router';
-
-const app_routes: Routes = [
-
-];
-
-export const app_routing = RouterModule.forRoot(app_routes);
